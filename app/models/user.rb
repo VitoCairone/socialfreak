3aa0223374f380fdde308a3575452f949cd5b3be
Vito Cairone <vitocairone@gmail.com>
2016-04-11 17:27:07 -0700

    authentication with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
 			user.name = auth_hash['info']['name']
 			user.location = auth_hash['info']['location']
 			user.image_url = auth_hash['info']['image']
-			user.url = auth_hash['info']['urls']['Twitter']
+			# user.url = auth_hash['info']['urls']['Twitter']
+			user.url = auth_hash['info']['urls'][user.provider.capitalize]
 			user.save!
 			# in a REAL app should check for and handle failure to save
 			user
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # for a REAL app use the environment variables, don't store key/secret in the file
   # provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+
   provider :twitter, 'BZZsihUiv4B9lKF72UQnbA5x9', 'PhVrrE8MU2ARuXxUjDqR0mV4oUvsGIRRtJUmzB3UJSNGYTh7KG'
+
+  provider :facebook, '1092342727475575', 'edeac296b58eac60d206a9da6b3b4477',
+  scope: 'public_profile', info_fields: 'id,name,link'
 end
